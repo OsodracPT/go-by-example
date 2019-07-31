69a1635033b93f5182ee93e4e9010481da760b0b
Pedro Cardoso <pe278@cam.ac.uk>
2019-07-31 23:43:59 +0100

    Creating the models

--- rest-api-mux/main.go
+++ rest-api-mux/main.go
@@ -7,6 +7,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author Struct
+type Author struct {
+	ID        string `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
 func main() {
 	//Init Router
 	r := mux.NewRouter()
