d023b2063e81cbfdac735dff4501b225e64f6a79
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-09-24 23:45:21 +0100

    Hello World with tests

---
+++ tests/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("World", ""))
+}
