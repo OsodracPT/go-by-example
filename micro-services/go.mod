e63d9267a1f67db7529ce5ae9fa2ed89f67d0fa3
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-03-31 11:42:46 +0100

    RESTful services

--- micro-services/go.mod
+++ micro-services/go.mod
@@ -1,3 +1,5 @@
 module micro-services
 
 go 1.14
+
+require golang.org/x/tools v0.0.0-20200331025713-a30bf2db82d4 // indirect
--- micro-services/src/main.go
+++ micro-services/src/main.go
@@ -12,12 +12,13 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodBye(l)
-	sm := http.NewServeMux()
 
-	sm.Handle("/", hh)
-	sm.Handle("goodbye", gh)
+	// create the handlers
+	ph := handlers.NewProducts(l)
+
+	// create a new serve mux and create a new route
+	sm := http.NewServeMux()
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
