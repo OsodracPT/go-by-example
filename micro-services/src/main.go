bd2a9aaa3ad339c791ded487f9a5641e490099fe
Pedro Cardoso <pedroosodracwork@gmail.com>
2020-04-07 18:52:47 +0100

    Gorilla framework for RESTful services

--- micro-services/src/main.go
+++ micro-services/src/main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
@@ -17,8 +19,15 @@ func main() {
 	ph := handlers.NewProducts(l)
 
 	// create a new serve mux and create a new route
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+	//sm.Handle("/products", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
